4664b17c7ec52f2f7e421ccb63629c1b370bbc36
sanjay0920 <sanjaybharathis18087@it.ssn.edu.in>
2022-08-19 17:27:59 +0530

    exercise2

---
+++ 19-08-2022/exercises 2/exercises 2/Program.cs
@@ -0,0 +1,254 @@
+﻿
+//problem1
+using System;
+namespace application
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int min;
+            int max;
+            Console.Write("Enter first number : ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b)
+                max = a;
+            else
+                max = b;
+
+            if (a < b)
+                min = a;
+            else
+                min = b;
+
+            Console.WriteLine("Minimum number = {0}", min);
+            Console.WriteLine("Maximum number = {0}", max);
+        }
+    }
+}
+
+//problem2
+using System;
+namespace application
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+            Console.Write("enter the 1st number:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter the 2nd number:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter the 3rd number:");
+            num3 = Convert.ToInt32(Console.ReadLine());
+            if(num1 > num3)
+            {
+                if (num1 > num2)
+                {
+                    Console.Write("1st number is greatest");
+                }
+                else
+                {
+                    Console.Write("3rd number is greatest");
+                }
+            }
+            else if (num2 > num3)
+                Console.Write("2nd number is greatest");
+            else
+                Console.Write("3rd number is greatest");
+
+        }
+    }
+}
+
+//problem3
+using System;
+namespace application
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string s1, s2;
+
+            Console.WriteLine("Enter the first character : ");
+            s1 = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Enter the second character : ");
+            s2 = Convert.ToString(Console.ReadLine());
+
+            if (s1 == s2)
+                Console.WriteLine("characters are equal");
+            else
+                Console.WriteLine("Characters are not equal");
+        }
+    }
+}
+//problem5
+using System;
+namespace application
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            int a, b;
+            Console.WriteLine("Enter the Two Numbers : ");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine("{0} is the Greatest Number", a);
+            }
+            else
+            {
+                Console.WriteLine("{0} is the Greatest Number ", b);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+//problem6
+using System;  
+namespace application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Year : ");
+            int Year = int.Parse(Console.ReadLine());
+            if (((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0)) Console.WriteLine("{0} is a Leap Year.", Year);
+            else Console.WriteLine("{0} is not a Leap Year.", Year);
+            Console.ReadLine();
+        }
+    }
+}
+
+//problem7
+using System;
+namespace application
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("input the marks of C:");
+            int C = Convert.ToInt32(Console.ReadLine());
+            Console.Write("input the marks of CPP:");
+            int CPP = Convert.ToInt32(Console.ReadLine());
+            Console.Write("input the marks of CSharp:");
+            int CSharp = Convert.ToInt32(Console.ReadLine());
+            double total = C + CPP + CSharp;
+            double per = total / 3.0;
+            string div;
+            if (per < 60)
+                div = "first";
+            else if (per < 60 && per >= 45)
+                div = "second";
+            else if (per < 45 && per >= 36)
+                div = "third";
+            else div = "fail";
+            Console.Write("grade:" + div);
+            Console.ReadLine();
+        }
+    }
+}
+//problem8
+
+using System;
+namespace application
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 6; i++)
+            {
+                for (int j = 1; j <= i; ++j)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+//problem9
+using System;
+namespace application
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int i, n, sum = 0;
+            Console.Write("enter number of terms : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("The odd numbers are :");
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write("{0} ", 2 * i - 1);
+                sum += 2 * i - 1;
+            }
+        }
+    }
+}
+
+//problem10
+using System;
+namespace application
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            int num, r, sum = 0, t;
+            Console.Write("Input a number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (t = num; t != 0; t = t / 10)
+            {
+                r = t % 10;
+                sum = sum * 10 + r;
+            }
+            Console.Write("The number in reverse order is : {0} ", sum);
+        }
+    }
+}
+
+
+//problem11
+using System;
+namespace application
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int i, sum = 0;
+            Console.Write("Numbers between 100 and 200 : ");
+            for (i = 101; i < 200; i++)
+            {
+
+
+                Console.Write("{0}  ", i);
+                sum += i;
+
+            }
+            Console.Write("The sum : {0} ", sum);
+        }
+    }
+}
