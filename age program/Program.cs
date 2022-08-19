7aeac1099b298787609e25a49f32823258416a77
sanjay0920 <sanjaybharathis18087@it.ssn.edu.in>
2022-08-19 17:14:04 +0530

    problem

---
+++ age program/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+public class age
+{
+    public static void Main()
+    {
+        int age;
+        Console.Write("Enter your age ");
+        age = Convert.ToInt32(Console.ReadLine());
+        Console.Write("You look younger than {0} ", age);
+    }
+}
