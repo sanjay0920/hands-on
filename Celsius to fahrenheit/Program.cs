bbc1003caead60f129828a1010f2679bc10eceed
sanjay0920 <sanjaybharathis18087@it.ssn.edu.in>
2022-08-19 17:25:47 +0530

    exercise

---
+++ Celsius to fahrenheit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class CelsiusToFahrenheit
+{
+    static void Main()
+    {
+        float celsius = 36;
+        Console.WriteLine("Temperature in celsius is: " + celsius);
+        float fahrenheit = ((celsius * 9) / 5) + 32;
+        Console.WriteLine("Temperature in Fahrenheit is: " + fahrenheit);
+        Console.ReadKey();
+    }
+}
---
+++ USD TO INR/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        double usd = 0;
+        double inr = 0;
+        double value = 0;
+
+        Console.Write("Enter amount in USD: ");
+        usd = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter the USD value :");
+        value = double.Parse(Console.ReadLine());
+
+        inr = usd * value;
+        Console.WriteLine("USD " + usd + "-> INR " + inr);
+    }
+}
