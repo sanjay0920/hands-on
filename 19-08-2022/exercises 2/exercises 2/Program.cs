775a8cf966c4bc0d2e34bfe24e6700275f9b9b51
sanjay0920 <sanjaybharathis18087@it.ssn.edu.in>
2022-08-20 13:29:54 +0530

    exercise2

--- 19-08-2022/exercises 2/exercises 2/Program.cs
+++ 19-08-2022/exercises 2/exercises 2/Program.cs
@@ -91,6 +91,39 @@ namespace application
         }
     }
 }
+
+//problem4
+
+using System;
+namespace application
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string str1;
+            string str2;
+            Console.Write("Enter a string: ");
+            str1 = Console.ReadLine();
+            Console.Write("Enter another string: ");
+            str2 = Console.ReadLine();
+
+
+
+            Console.WriteLine("output");
+            if (str1.ToUpper() == str2.ToUpper())
+                Console.WriteLine("\"{0}\" and \"{1}\" are equal", str1, str2);
+            else
+                Console.WriteLine("\"{0}\" and \"{1}\" are not equal", str1, str2);
+
+            Console.ReadLine();
+
+        }
+    }
+
+}
+
+
 //problem5
 using System;
 namespace application
