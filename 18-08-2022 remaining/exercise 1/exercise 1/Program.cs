252b9cfc1cd708bb741729fda1421560a8e47342
sanjay0920 <sanjaybharathis18087@it.ssn.edu.in>
2022-08-22 08:52:08 +0530

    exercise1

--- 18-08-2022 remaining/exercise 1/exercise 1/Program.cs
+++ 18-08-2022 remaining/exercise 1/exercise 1/Program.cs
@@ -164,3 +164,47 @@ public class Distance
 
     }
 }
+
+//problem16
+
+using System;
+namespace application
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Hello";
+            String emplynm = "";
+            int deptnum = 0;
+            string name = String.Empty;
+            Console.WriteLine("Enter the name");
+            name = Console.ReadLine();
+            if (!string.IsNullOrEmpty(name))
+            {
+                string greet = string.Concat(s, name);
+                Console.Write(greet);
+                Console.WriteLine("enter the deptnum");
+                deptnum = Convert.ToInt32(Console.ReadLine());
+                if (deptnum != 0 && (deptnum >= 1 || deptnum < 10))
+                {
+                    Console.WriteLine("thanks for input");
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Console.WriteLine("Enter the employee name");
+                        emplynm = Console.ReadLine();
+                        Console.WriteLine("enter his designation");
+                        Console.ReadLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("enter the valid deptnum");
+                }
+            }
+
+        }
+    }
+}
+
+
